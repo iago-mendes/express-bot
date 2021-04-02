b5f2a167d46bcc388107d51b58c3c6b120579cf4
Iago <iagobrazmendes@gmail.com>
2021-04-02 17:19:45 -0300

    connect to telegram api

---
+++ src/services/telegram/api.ts
@@ -0,0 +1,12 @@
+import axios from 'axios'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const token = process.env.TELEGRAM_TOKEN
+
+const api = axios.create(
+	{
+		baseURL: `https://api.telegram.org/bot${token}/`,
+	})
+
+export default api
